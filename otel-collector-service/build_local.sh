fbb0c6dc37d6f27285579f4ce114d05af43ede1e
David G Andalcio <davega9@gmail.com>
2025-07-04 17:24:43 -0500

    removed the build local scripts

--- otel-collector-service/build_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-OTEL_COLLECTOR_CONTRIB_VERSION=$(yq '.otel-collector-contrib.version' ./version.yaml)
-echo "${OTEL_COLLECTOR_CONTRIB_VERSION}"
-
-docker build --no-cache -t local/otel-collector-service:latest --build-arg otel_collector_version="${OTEL_COLLECTOR_CONTRIB_VERSION}" .
